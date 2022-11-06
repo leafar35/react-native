d118f71a8ba1b1b5be7e2bca912695e5280af045
Rafael Vianini <rafael_vianini@yahoo.com.br>
2022-11-06 19:05:34 -0300

    cronometro backup

--- walletApp/App.js
+++ walletApp/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { 
   Text, 
   View, 
-  StyleSheet, 
-  TextInput, 
-  Button, 
+  StyleSheet,
   Image,
   TouchableOpacity
 } from 'react-native';
@@ -14,46 +12,67 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      textPhrase: 'Frase 1',
-      img: require('./src/biscoito.png')
-    };
+      number: 0,
+      textButton: 'Começar',
+      lastTime: 0
+    }
 
-    this.break = this.break.bind(this);
+    this.time = null;
+    this.start = this.start.bind(this);
+    this.clear = this.clear.bind(this);
 
-    this.phrases = [
-      'frase 1',
-      'frase 2',
-      'frase 3',
-      'frase 4',
-      'frase 5',
-      'frase 6',
-      'frase 7',
-    ];
   }
 
-  break(){
-    let number = Math.floor(Math.random() * this.phrases.length);
-    this.setState({
-      textPhrase: '"' + this.phrases[number] + '"',
-      img: require('./src/biscoitoAberto.png')
-    });
+  start(){
+
+    if(this.time !== null){
+      this.setState({textButton: 'Começar'});
+      clearInterval(this.time);
+      return this.time = null;
+    }
+
+    this.time = setInterval(() => {
+      this.setState({textButton: 'Parar'});
+      this.setState({number: this.state.number + 0.01});
+    },100)
+  }
+
+  clear(){
+    if(this.time !== null){
+      this.setState({lastTime: this.state.number})
+      this.setState({textButton: 'Começar'});
+      clearInterval(this.time);
+      this.time = null;
+    }
+    this.setState({number: 0});
   }
 
   render(){
     return(
-      <View style={style.container}>
+      <View style={style.container}>        
+
         <Image 
-          source={this.state.img}
-          style={style.img} 
-        />
+          source={require('./src/cronometro.png')} 
+          style={style.stopwatch}
+        />  
 
-        <Text style={style.text}> {this.state.textPhrase} </Text>
+        <Text style={style.timer}>{this.state.number.toFixed(1)}</Text>
 
-        <TouchableOpacity style={style.btn} onPress={this.break}>
-          <View style={style.btnArea}>
-            <Text style={style.textbtn}>Abrir Biscoito</Text>
-          </View>
-        </TouchableOpacity>
+        <View style={style.btnArea}>
+
+          <TouchableOpacity style={style.btn} onPress={this.start}>
+            <Text style={style.btnText}>{this.state.textButton}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.btn} onPress={this.clear}>
+            <Text style={style.btnText}>LIMPAR</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={style.areaLast}>
+          <Text style={style.textTime}>Ultimo Tempo {this.state.lastTime.toFixed(2)}</Text>
+        </View>
 
       </View>
     );
@@ -65,36 +84,40 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 50
+    backgroundColor: '#00aeef'
   },
-  img: {
-    width: 250,
-    height:250,
-  },
-  text: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  btn: {
-    width: 250,
-    height: 50,
-    borderWidth: 2,
-    borderColor:'#dd7b22',
-    borderRadius: 25
+  timer: {
+    marginTop: -160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold'
   },
   btnArea: {
-    flex: 1,
     flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
   },
-  textBtn: {
-    fontSize: 18,
+  btnText: {
+    fontSize: 20,
     fontWeight: 'bold',
-    color: '#dd7b2'
+    color: '#00aeef'
+  },
+  areaLast: {
+    marginTop: 40,    
+  },
+  textTime: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#fff'
   }
 });
 
