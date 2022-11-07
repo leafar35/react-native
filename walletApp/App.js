137a54c85979e92ee5ac209f5603ff0f22751bf3
Rafael Vianini <rafael_vianini@yahoo.com.br>
2022-11-07 17:13:21 -0300

    Slider estudo

--- walletApp/App.js
+++ walletApp/App.js
@@ -4,42 +4,35 @@ import {
   View, 
   StyleSheet,
 } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {id:0, name: 'pizza 1', valor: 39.50},
-        {id:1, name: 'pizza 4', valor: 39.50},
-        {id:2, name: 'pizza 3', valor: 39.50},
-        {id:3, name: 'pizza 6', valor: 39.50},        
-      ]
+      value: 0
     }
   }
 
   render(){
-
-    let pizzasitem = this.state.pizzas.map((v,k) => {
-      return <Picker.Item key={v.id} value={v.id} label={v.name} />
-    });
-
     return(
       <View style={style.container}>
-        <Text style={style.logo}>Menu Pizza</Text>
 
-        <Picker
-          selectedValue={this.state.pizza}
-          onValueChange={(itemValue, itemIndex) => this.setState({pizza: itemValue})}
-        >
-          {pizzasitem}
-        </Picker>
 
-        <Text style={style.pizzas}>Você escolheu.: {this.state.pizzas[this.state.pizza].name}</Text>
-        <Text style={style.pizzas}>R$.: 59,99</Text>
+        <Slider 
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(valueSelected) => this.setState({value: valueSelected})}
+          value={this.state.value}
+          minimumTrackTintColor='#00FF00'
+          maximumTrackTintColor='#FF0000'
+        />
+
+        <Text style={{textAlign: 'center', fontSize: 30}}>
+          {this.state.value.toFixed(0)} KG
+        </Text>
+
       </View>
     );
   }
@@ -48,17 +41,7 @@ class App extends Component {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 35
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  pizzas: {
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center'
+    marginTop: 80
   }
 });
 
