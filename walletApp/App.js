57b3cd94d813c75455af57d06c6eb01ca46f2953
Rafael Vianini <rafael_vianini@yahoo.com.br>
2022-11-10 21:34:17 -0300

    localstorage react native

--- walletApp/App.js
+++ walletApp/App.js
@@ -4,70 +4,63 @@ import {
   Text, 
   View, 
   StyleSheet,
-  Switch,
   TextInput,
-  Button
+  TouchableOpacity,
+  Keyboard
 } from 'react-native';
-import Slider from '@react-native-community/slider';
+//import AsyncStorage from '@react-native-community/async-storage';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      gender: 'M',
-      Limit: 0,
-      Studenty: false,      
+      input: '',
+      name: 'Rafael'
     }
-
-    this.sendform = this.sendform.bind(this);
+    this.saveName = this.saveName.bind(this);
   }
 
-  sendform(){
-    if(this.state.name.trim() == ''){
-      return alert('Informe o Nome');
+  //sempre é chamado quando a tela for montada ciclo de vida do react native
+  componentDidMount(){
+
+  }
+
+  //toda vez que o state é atualizado react native
+  componentDidUpdate(prevProps, prevState){
+    const name = this.state.name;
+    if(prevState != name){
+      //localStorage.setItem('name', this.state.name);
     }
-    if(this.state.age.trim() == ''){
-      return alert('Informe a idade');
-    }
-    if(this.state.Limit == 0){
-      return alert('Selecione o limite!');
-    }
-    alert('Conta aberta com sucesso!');
+  }
+
+  saveName(){
+    this.setState({
+      name: this.state.input
+    });
+    alert(this.state.name);
+    Keyboard.dismiss();
   }
 
   render(){
     return(
       <View style={style.container}>
-        <TextInput style={style.input} placeholder='Informe seu nome' value={this.state.name} />
-        <TextInput style={style.input} placeholder='Informe sua idade' value={this.state.age} />
-        <Picker
-          selectedValue={this.state.gender}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({gender: itemValue})
-          }>
-          <Picker.Item label="Masculino" value="M" />
-          <Picker.Item label="Feminino" value="F" />
-        </Picker>
-        <Slider 
-          minimumValue={0}
-          maximumValue={100}
-          onValueChange={(valueSelected) => this.setState({Limit: valueSelected})}
-          value={this.state.Limit}
-          minimumTrackTintColor='#00FF00'
-          maximumTrackTintColor='#FF0000'
-        />
-        <View style={style.areaSwitch}>
-          <Text style={{width: 97, marginTop: 5}}>Não Estudante</Text>
-          <Switch style={{width: 55}}
-            onValueChange={(valueChange) => this.setState({Studenty: valueChange})}
-            value={this.state.Studenty}
+        <View style={style.viewInput}>
+
+          <TextInput 
+            style={style.input}
+            value={this.state.input}
+            onChangeText={(text) => this.setState({input: text})}
+            underlineColorAndroid="transparent"
           />
-          <Text style={{width: 70, marginTop: 5}}>Estudante</Text>
-        </View>
-        <Button style={style.btn} title='Abrir Conta' name="Enviar" onPress={() => this.sendform()} />
+          <TouchableOpacity onPress={this.saveName}>
+            <Text style={style.btn}>+</Text>
+          </TouchableOpacity>
+
+        </View>   
+        
+        <Text style={style.name}>{this.state.name}</Text>
+
       </View>
     );
   }
@@ -77,24 +70,30 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 80,
-    padding: 50
+    alignItems: 'center',
   },
-  btn: {
-    flex:1,
-    width: 250,
-    flexDirection: 'column',
-  },
-  areaSwitch: {
-    flex: 1,
+  viewInput: {
     flexDirection: 'row',
-    marginTop: 10
+    alignItems: 'center',
   },
-  input: {
+  input:{
+    width: 350,
     height: 40,
-    margin: 12,
+    borderColor: '#000',
     borderWidth: 1,
     padding: 10,
   },
+  btn: {
+    backgroundColor: '#222',
+    color: '#fff',
+    height: 40,
+    padding: 10,
+    marginLeft: 4
+  },
+  name: {
+    fontSize: 30,
+    textAlign: 'center'
+  }
 });
 
 export default App;
