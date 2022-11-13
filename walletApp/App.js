28f93926c0a19437a056301c0ebe7ab5838bc2c9
Rafael Vianini <rafael_vianini@yahoo.com.br>
2022-11-13 15:51:58 -0300

    animações

--- walletApp/App.js
+++ walletApp/App.js
@@ -1,73 +1,202 @@
-import { Picker } from '@react-native-picker/picker';
 import React, { Component } from 'react';
 import { 
   Text, 
   View, 
   StyleSheet,
-  Switch,
-  TextInput,
-  Button
+  Animated,
+  TouchableOpacity
 } from 'react-native';
-import Slider from '@react-native-community/slider';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      gender: 'M',
-      Limit: 0,
-      Studenty: false,      
+      widthAnimate: new Animated.Value(150),
+      heightAnimate: new Animated.Value(50),
+      opacityAnimate: new Animated.Value(0),
+      opacityTextAnimate: new Animated.Value(0),
+      heightAnimatel: new Animated.Value(0)
     }
 
-    this.sendform = this.sendform.bind(this);
+
+    /*
+    Animated.loop(
+
+      Animated.sequence([
+
+        Animated.timing(
+          this.state.widthAnimate,{
+            toValue: 250,
+            duration: 700
+          }
+        ),
+
+        Animated.timing(
+          this.state.widthAnimate,{
+            toValue: 150,
+            duration: 700
+          }
+        )
+
+      ])
+      
+    ).start();
+    */ 
+
+    /*
+    Animated.sequence([
+      Animated.timing(
+        this.state.opacityAnimate,{
+          toValue: 1,
+          duration: 1500
+        }
+      ),
+      Animated.parallel([
+        Animated.timing(
+          this.state.widthAnimate,{
+            toValue: 300,
+            duration: 2000
+          }
+        ),
+        Animated.timing(
+          this.state.heightAnimate,{
+            toValue: 200,
+            duration: 2000
+          }
+        ),
+      ])
+    ]).start();
+    */
+
+    /*
+    Animated.parallel([
+      Animated.timing(
+        this.state.widthAnimate,{
+          toValue: 300,
+          duration: 2000
+        }
+      ),
+      Animated.timing(
+        this.state.heightAnimate,{
+          toValue: 200,
+          duration: 2000
+        }
+      ),
+      Animated.timing(
+        this.state.opacityAnimate,{
+          toValue: 0,
+          duration: 2000
+        }
+      ),
+    ]).start();
+    */
+
+
+    /*
+    Animated.sequence([
+      Animated.timing(
+        this.state.widthAnimate,{
+          toValue: 300,
+          duration: 2000
+        }
+      ),
+      Animated.timing(
+        this.state.heightAnimate,{
+          toValue: 200,
+          duration: 2000
+        }
+      ),
+      Animated.timing(
+        this.state.opacityAnimate,{
+          toValue: 1,
+          duration: 1500
+        }
+      ),
+    ]).start();
+    */
+
+    /*
+    Animated.timing(
+      this.state.widthAnimate,{
+        toValue: 300,
+        duration: 2000
+      }
+    ).start();
+    */
+
+    Animated.timing(
+      this.state.heightAnimatel,{
+        toValue: 100,
+        duration: 5000
+      }
+    ).start();
+
+    this.loadGrapth = this.loadGrapth.bind(this);
+
   }
 
-  sendform(){
-    if(this.state.name.trim() == ''){
-      return alert('Informe o Nome');
-    }
-    if(this.state.age.trim() == ''){
-      return alert('Informe a idade');
-    }
-    if(this.state.Limit == 0){
-      return alert('Selecione o limite!');
-    }
-    alert('Conta aberta com sucesso!');
+  loadGrapth(){
+    Animated.sequence([
+      Animated.timing(
+        this.state.opacityAnimate,{
+          toValue: 1,
+          duration: 400
+        }
+      ),
+      Animated.timing(
+        this.state.heightAnimate,{
+          toValue: 300,
+          duration: 1000
+        }
+      ),
+      Animated.timing(
+        this.state.opacityTextAnimate,{
+          toValue: 1,
+          duration: 400
+        }
+      ),
+    ]).start();
   }
 
   render(){
+    let porcentAnimate = this.state.heightAnimatel.interpolate({
+      inputRange: [0,100],
+      outputRange: ['0%','100%']
+    })
     return(
       <View style={style.container}>
-        <TextInput style={style.input} placeholder='Informe seu nome' value={this.state.name} />
-        <TextInput style={style.input} placeholder='Informe sua idade' value={this.state.age} />
-        <Picker
-          selectedValue={this.state.gender}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({gender: itemValue})
-          }>
-          <Picker.Item label="Masculino" value="M" />
-          <Picker.Item label="Feminino" value="F" />
-        </Picker>
-        <Slider 
-          minimumValue={0}
-          maximumValue={100}
-          onValueChange={(valueSelected) => this.setState({Limit: valueSelected})}
-          value={this.state.Limit}
-          minimumTrackTintColor='#00FF00'
-          maximumTrackTintColor='#FF0000'
-        />
-        <View style={style.areaSwitch}>
-          <Text style={{width: 97, marginTop: 5}}>Não Estudante</Text>
-          <Switch style={{width: 55}}
-            onValueChange={(valueChange) => this.setState({Studenty: valueChange})}
-            value={this.state.Studenty}
-          />
-          <Text style={{width: 70, marginTop: 5}}>Estudante</Text>
+
+        <View style={{height:80, alignItems: 'center', justifyContent: 'center', flexDirection: 'row', backgroundColor: '#4169E1'}}>
+          <TouchableOpacity onPress={this.loadGrapth}>
+            <Text style={{fontSize: 25, color:'#ffffff'}}>Gerar Grafico</Text>
+          </TouchableOpacity>
         </View>
-        <Button style={style.btn} title='Abrir Conta' name="Enviar" onPress={() => this.sendform()} />
+
+        <Animated.View style={{backgroundColor: '#4169E1', width: porcentAnimate, height: 25, justifyContent: 'center', marginTop: '55%'}}>
+
+        </Animated.View>
+
+        <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'}}>
+          <Animated.View style={{opacity: this.state.opacityTextAnimate}}>
+            <Text>Vendas</Text>
+          </Animated.View>
+          <Animated.View style={[
+            style.children, 
+            { 
+              width: this.state.widthAnimate, 
+              height: this.state.heightAnimate, 
+              backgroundColor: '#FF0000',
+              justifyContent: 'center',
+              opacity: this.state.opacityAnimate,
+              //borderRadius: 25 animacoes comentadas
+            } 
+            ]}>
+            <Text style={style.text}>R$ 150,00</Text>
+          </Animated.View>
+
+        </View>        
+
       </View>
     );
   }
@@ -76,25 +205,18 @@ class App extends Component {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 80,
-    padding: 50
+    marginTop: 55
   },
-  btn: {
-    flex:1,
-    width: 250,
-    flexDirection: 'column',
+  children: {
+    backgroundColor: '#4169E1',
+    justifyContent: 'center',
+    marginBottom: 55
   },
-  areaSwitch: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 10
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+  text: {
+    color: "#FFFFFF",
+    fontSize: 22,
+    textAlign: 'center',    
+  }
 });
 
 export default App;
