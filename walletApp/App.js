3e5d38b6ddbe9d919604ee218c674e0508bbff8a
Rafael Vianini <rafael_vianini@yahoo.com.br>
2022-11-14 15:32:07 -0300

    hoocks aprendendo

--- walletApp/App.js
+++ walletApp/App.js
@@ -1,100 +1,99 @@
-import { Picker } from '@react-native-picker/picker';
-import React, { Component } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { 
   Text, 
   View, 
   StyleSheet,
-  Switch,
   TextInput,
-  Button
+  TouchableOpacity,
+  Keyboard,
 } from 'react-native';
-import Slider from '@react-native-community/slider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-class App extends Component {
+export default function App() {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      gender: 'M',
-      Limit: 0,
-      Studenty: false,      
+  const [name,setName] = useState('');
+  const [input,setInput] = useState('');
+  const nameInput = useRef(null);
+
+  //component did amount
+  useEffect(() => {
+
+    async function getStorage(){
+      const names = await AsyncStorage.getItem('names');
+      if(names)
+        setName(names);      
     }
 
-    this.sendform = this.sendform.bind(this);
+    getStorage();
+
+  } ,[]);
+
+  useEffect(() => {
+
+    async function saveStorage(){
+      await AsyncStorage.setItem('names', name);
+    }
+
+    saveStorage();
+
+  },[name])
+
+  function updateNmae(){
+    setName(input);
+    setInput('');
+    Keyboard.dismiss();
   }
 
-  sendform(){
-    if(this.state.name.trim() == ''){
-      return alert('Informe o Nome');
-    }
-    if(this.state.age.trim() == ''){
-      return alert('Informe a idade');
-    }
-    if(this.state.Limit == 0){
-      return alert('Selecione o limite!');
-    }
-    alert('Conta aberta com sucesso!');
+  function newName(){
+    nameInput.current.focus();
   }
 
-  render(){
-    return(
-      <View style={style.container}>
-        <TextInput style={style.input} placeholder='Informe seu nome' value={this.state.name} />
-        <TextInput style={style.input} placeholder='Informe sua idade' value={this.state.age} />
-        <Picker
-          selectedValue={this.state.gender}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({gender: itemValue})
-          }>
-          <Picker.Item label="Masculino" value="M" />
-          <Picker.Item label="Feminino" value="F" />
-        </Picker>
-        <Slider 
-          minimumValue={0}
-          maximumValue={100}
-          onValueChange={(valueSelected) => this.setState({Limit: valueSelected})}
-          value={this.state.Limit}
-          minimumTrackTintColor='#00FF00'
-          maximumTrackTintColor='#FF0000'
-        />
-        <View style={style.areaSwitch}>
-          <Text style={{width: 97, marginTop: 5}}>Não Estudante</Text>
-          <Switch style={{width: 55}}
-            onValueChange={(valueChange) => this.setState({Studenty: valueChange})}
-            value={this.state.Studenty}
-          />
-          <Text style={{width: 70, marginTop: 5}}>Estudante</Text>
-        </View>
-        <Button style={style.btn} title='Abrir Conta' name="Enviar" onPress={() => this.sendform()} />
-      </View>
-    );
-  }
+  //Use mesmo ajuda no uso de memoria, não chamar semprea a cada mudança de valor e sim somente quando o valor name e alterado nesse contexto.
+  const lengthName = useMemo(() => { 
+    return name.length 
+  },[name]);
+
+  return (
+    <View style={style.container}>
+
+      <TextInput 
+        placeholder='Digite novo nome'
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        ref={nameInput}
+      />
+
+      <TouchableOpacity style={style.btn} onPress={updateNmae}>
+        <Text style={style.btntext}>Trocar Nome</Text>  
+      </TouchableOpacity>
+
+      <Text style={style.text}>{name}</Text>
+      <Text style={style.text}>Tem {lengthName} letras</Text>
+
+      <TouchableOpacity style={style.btn} onPress={newName}>
+        <Text style={style.btntext}>Novo Nome</Text>  
+      </TouchableOpacity>
+
+    </View>
+  );
+
 }
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 80,
-    padding: 50
+  },
+  text: {
+    fontSize: 35,
+    color: '#000'
   },
   btn: {
-    flex:1,
-    width: 250,
-    flexDirection: 'column',
+    backgroundColor: '#222',
+    alignItems: 'center',
   },
-  areaSwitch: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 10
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+  btntext: {
+    fontSize: 15,
+    color: '#fff'
+  }
 });
-
-export default App;
